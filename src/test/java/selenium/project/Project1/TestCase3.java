4ae5267385447573d45d223814053f7a3ef88e93
Sanjivani Rathod <sanjivanijadhav228@gmail.com>
2024-09-21 16:24:09 +0530

    testcase3

--- src/test/java/selenium/project/Project1/TestCase3.java
+++ src/test/java/selenium/project/Project1/TestCase3.java
@@ -1,20 +1,23 @@
 package selenium.project.Project1;
 
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
 import org.testng.annotations.Test;
 
 public class TestCase3 extends Launch_Quit
 {
 	@Test
-	public void valid_login() throws InterruptedException
+	public void valid_login() throws InterruptedException, EncryptedDocumentException, IOException
 	{
 		Amazon_HomePage ah = new Amazon_HomePage (driver);
 		ah.account_list(driver);
 		ah.signin(driver);
 		Amazon_LoginPage al = new Amazon_LoginPage( driver);
-		al.email();
+		al.enter_email();
 		al.continue_();
-		al.pass();
-		al.signin();
+		al.enterPassword();
+		al.signin(driver);
 	}
 
 }
